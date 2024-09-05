229126df704ef21eafa03a45bf1e9dcbe5776daa
Danilo Santos <danilo@devstudio.com.br>
2024-09-05 09:53:44 -0300

    create cardpost

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,10 @@
+import styles from './aside.module.css';
+import Image from 'next/image';
+
+import logo from './logo.png';
+
+export const Aside = () => {
+  return (<aside className={styles.aside}>
+      <Image src={logo} alt='Logo da Code Connect' />
+  </aside>)
+}
